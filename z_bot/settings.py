e181735ab942d1936c00cbca5922c47bc1bc9f1a
zjjaxx <2495893564@qq.com>
2025-02-11 21:00:15 +0800

    prod

--- z_bot/settings.py
+++ z_bot/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-(^cq*!gf*uue-=72t@0htsl0_ubr57$_k46bo$-ga2!$!-r6%)"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
